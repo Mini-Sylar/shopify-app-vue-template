23acff12520b6912a6cb1c612ba238aebe01b3e2
Mini-Sylar <minisylar3@gmail.com>
2023-06-19 12:47:22 +0000

    Add app bridge and authenticated fetch hook

---
+++ web/frontend/src/helpers/appBridge.js
@@ -0,0 +1,18 @@
+import { createApp } from '@shopify/app-bridge'
+
+const host = new URLSearchParams(location.search).get('host') || window.__SHOPIFY_DEV_HOST
+
+window.__SHOPIFY_DEV_HOST = host
+
+export const shopifyPlugin = {
+  install: (app) => {
+    const appBridge = createApp({
+      apiKey: process.env.SHOPIFY_API_KEY,
+      host: host,
+      forceRedirect: true
+    })
+
+    app.config.globalProperties.$appBridge = appBridge
+    app.provide('useAppBridge', appBridge)
+  }
+}
---
+++ web/frontend/src/helpers/vueAuthenticatedFetch.js
@@ -0,0 +1,39 @@
+import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { createApp } from "@shopify/app-bridge";
+import { Redirect } from "@shopify/app-bridge/actions";
+
+export function useAuthenticatedFetchVue() {
+  const host =
+    new URLSearchParams(location.search).get("host") ||
+    window.__SHOPIFY_DEV_HOST;
+
+  window.__SHOPIFY_DEV_HOST = host;
+  const appBridge = createApp({
+    apiKey: process.env.SHOPIFY_API_KEY,
+    host: host,
+    forceRedirect: true,
+  });
+  const app = appBridge;
+  const fetchFunction = authenticatedFetch(app);
+
+  return async (uri, options) => {
+    const response = await fetchFunction(uri, options);
+    checkHeadersForReauthorization(response.headers, app);
+    return response;
+  };
+}
+
+function checkHeadersForReauthorization(headers, app) {
+  if (headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1") {
+    const authUrlHeader =
+      headers.get("X-Shopify-API-Request-Failure-Reauthorize-Url") ||
+      `/api/auth`;
+    const redirect = Redirect.create(app);
+    redirect.dispatch(
+      Redirect.Action.REMOTE,
+      authUrlHeader.startsWith("/")
+        ? `https://${window.location.host}${authUrlHeader}`
+        : authUrlHeader
+    );
+  }
+}
