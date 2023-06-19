c6bbcb8756b041fc8583010fba42ed1c9af0eb0e
Mini-Sylar <minisylar3@gmail.com>
2023-06-19 13:10:02 +0000

    Clean up default stylings

---
+++ web/frontend/src/views/ExitIframeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        ExitIframeView
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
--- web/frontend/src/views/HomeView.vue
+++ web/frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Home View
   </main>
 </template>
