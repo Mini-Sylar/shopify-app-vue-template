973cbc74dfffe7979954576814ca33568c9735e3
Mini-Sylar <minisylar3@gmail.com>
2024-04-15 11:29:34 +0000

    Set up i18n

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -3,11 +3,22 @@ import { ShopifyAppBridge } from '@/plugins/appBridge.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+import en from './locales/en.json'
+
+const i18n = createI18n({
+  locale: 'en',
+  messages: {
+    en
+  }
+})
+
 const app = createApp(App)
+app.use(i18n)
 app.use(ShopifyAppBridge)
 app.use(createPinia())
 app.use(router)
