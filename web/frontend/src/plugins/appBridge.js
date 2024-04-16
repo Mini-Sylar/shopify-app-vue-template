79698a48633b9b48d5dbfc6ded4e8d047ca6a4fc
Mini-Sylar <minisylar3@gmail.com>
2024-04-16 13:55:21 +0000

    chore: Update package name and version

--- web/frontend/src/plugins/appBridge.js
+++ web/frontend/src/plugins/appBridge.js
@@ -1,10 +1,12 @@
 // @ts-nocheck
 
+
 import { createApp } from '@shopify/app-bridge'
 export const initAppBridge = () => {
   const host = new URLSearchParams(location.search).get('host') || window.__SHOPIFY_DEV_HOST
   window.__SHOPIFY_DEV_HOST = host
   return createApp({
+    // eslint-disable-next-line no-undef
     apiKey: process.env.SHOPIFY_API_KEY || '',
     host: host,
     forceRedirect: true
