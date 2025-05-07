d961617a1c4d66580e17cbccf403d242692cce04
Mini-Sylar <minisylar3@gmail.com>
2025-05-07 12:57:46 +0000

    style: add eslint directive for empty object type in env.d.ts

--- web/client/env.d.ts
+++ web/client/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="@shopify/app-bridge-types" />
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ImportMetaEnv {
   // more env variables...
 }
